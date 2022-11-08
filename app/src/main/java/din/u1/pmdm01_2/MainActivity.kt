760e4950d6a17192d79ddaba442d6dcbcb107ceb
Juan Manuel <jcumlop667@g.educaand.es>
2022-11-08 21:19:26 +0100

    Initial commit

---
+++ app/src/main/java/din/u1/pmdm01_2/MainActivity.kt
@@ -0,0 +1,56 @@
+package din.u1.pmdm01_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "Button clicked!",
+            Toast.LENGTH_SHORT).show()
+        val randomInt = (1..6).random()
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        if (resultText.text == "Hello World!") {
+            resultText.text = "1"
+        } else {
+            // Mi solución
+            if (resultText.text.toString().toInt() < 6) {
+                resultText.text = (resultText.text.toString().toInt() + 1).toString()
+            }
+
+            /*
+             if (resultText.text.toString().toInt() < 6) {
+                resultText.text = (resultText.text.toString().toInt().plus(1)).toString()
+            }
+            */
+
+            // Solución Original
+            /*var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }*/
+        }
+    }
+}
