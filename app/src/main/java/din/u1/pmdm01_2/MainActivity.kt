34de8b25b86dc6fdf7a286cdf5212eaf593f2573
Juan Manuel <jcumlop667@g.educaand.es>
2022-11-15 20:21:38 +0100

    Actualización de la App que sustituye los números por dados y elimina la función countUp

--- app/src/main/java/din/u1/pmdm01_2/MainActivity.kt
+++ app/src/main/java/din/u1/pmdm01_2/MainActivity.kt
@@ -3,54 +3,40 @@ package din.u1.pmdm01_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Get the Button view from the layout and assign a click listener to it
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener { countUp() }
+        // Variables lateinit relativas a los dos dados
+        // Para el segundo dado, tendremos que crear otra ImageView en activity_main.xml
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "Button clicked!",
-            Toast.LENGTH_SHORT).show()
-        val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
     }
 
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
+    private fun getRandomDiceImage() : Int {
+        val randomInt = (1..6).random()
 
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        } else {
-            // Mi solución
-            if (resultText.text.toString().toInt() < 6) {
-                resultText.text = (resultText.text.toString().toInt() + 1).toString()
-            }
-
-            /*
-             if (resultText.text.toString().toInt() < 6) {
-                resultText.text = (resultText.text.toString().toInt().plus(1)).toString()
-            }
-            */
-
-            // Solución Original
-            /*var resultInt = resultText.text.toString().toInt()
-
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }*/
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
 }
